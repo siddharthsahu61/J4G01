bb4981119a6bb595a62c1103eab5e54dd5e02262
siddharthsahu61 <49867103+siddharthsahu61@users.noreply.github.com>
2022-03-15 17:32:09 +0530

    Add files via upload

---
+++ Siddharth Sahu/Book.java
@@ -0,0 +1,18 @@
+
+public class Book {
+
+	public int bookPrice;
+	public String bookName,bookAuthor;
+	
+	public Book(String bookName,String bookAuthor,int bookPrice){
+		this.bookName = bookName;
+		this.bookAuthor = bookAuthor;
+		this.bookPrice = bookPrice;
+	}
+	
+	public void displayBook() {
+		System.out.println(bookName +" "+bookAuthor+" "+bookPrice);
+	}
+	
+	
+}
---
+++ Siddharth Sahu/SumAvg.java
@@ -0,0 +1,21 @@
+public class SumAvg {
+	
+	static int sum=0;
+
+	public static int sumNo(int[] numberArray) {
+		
+		for(int number:numberArray) {
+			sum+=number;
+		}
+		
+		return sum;}
+	
+	public static int avgNo(int[] numberArray) {
+		
+		for(int number:numberArray) {
+			sum+=number;
+		}
+		
+		return (sum/numberArray.length);}
+	
+}
